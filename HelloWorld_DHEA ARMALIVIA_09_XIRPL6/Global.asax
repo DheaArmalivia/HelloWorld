c429d6dee50366c316da7c8dbec3975c83de5aea
Dhea Armalivia <daffyderr@gmail.com>
2017-01-14 08:07:11 +0700

    hello world

---
+++ HelloWorld_DHEA ARMALIVIA_09_XIRPL6/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="HelloWorld_DHEA_ARMALIVIA_09_XIRPL6.MvcApplication" Language="C#" %>
---
+++ HelloWorld_DHEA ARMALIVIA_09_XIRPL6/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(HelloWorld_DHEA_ARMALIVIA_09_XIRPL6.Startup))]
+namespace HelloWorld_DHEA_ARMALIVIA_09_XIRPL6
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
